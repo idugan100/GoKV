f07f96a7da8c0e1d1a9e495f548aa4335a80a564
idugan100 <isaacd4444@gmail.com>
2024-05-08 23:05:24 -0400

    upping tests

--- main_test.go
+++ main_test.go
@@ -52,6 +52,36 @@ func TestHandleGetSetCommands(t *testing.T) {
 	}
 }
 
+func TestHandleInvalidSetCommand(t *testing.T) {
+	conn := getConnectionMock("*1\r\n$3\r\nset\r\n")
+
+	handleConnection(conn)
+
+	if !strings.Contains(conn.String(), "incorrect") {
+		t.Errorf("expected response of incorrect number of args got response of %s", conn.String())
+	}
+}
+
+func TestHandleInvalidGetCommand(t *testing.T) {
+	conn := getConnectionMock("*1\r\n$3\r\nget\r\n")
+
+	handleConnection(conn)
+
+	if !strings.Contains(conn.String(), "incorrect") {
+		t.Errorf("expected response of incorrect number of args got response of %s", conn.String())
+	}
+}
+
+func TestHandleGetCommandOnMissingKey(t *testing.T) {
+	conn := getConnectionMock("*2\r\n$3\r\nget\r\n$3\r\nmissing\r\n")
+
+	handleConnection(conn)
+
+	if !strings.Contains(conn.String(), "_\r\n") {
+		t.Errorf("expected response of incorrect number of args got response of %s", conn.String())
+	}
+}
+
 func TestHandlePingCommand(t *testing.T) {
 	conn := getConnectionMock("*1\r\n$4\r\nPING\r\n")
 
@@ -72,16 +102,6 @@ func TestHandleLolWutCommand(t *testing.T) {
 	}
 }
 
-func TestHandleSetCommendError(t *testing.T) {
-	conn := getConnectionMock("*1\r\n$3\r\nset\r\n")
-
-	handleConnection(conn)
-
-	if !strings.Contains(conn.String(), "incorrect") {
-		t.Errorf("expected response of incorrect number of args got response of %s", conn.String())
-	}
-}
-
 func TestHandleInvalidCommand(t *testing.T) {
 	conn := getConnectionMock("*1\r\n$3\r\n123\r\n")
 
@@ -98,6 +118,16 @@ func TestHandleIndvalidCommand(t *testing.T) {
 	handleConnection(conn)
 
 	if !strings.Contains(conn.String(), "command not found") {
+		t.Errorf("expected response of command not found got response of %s", conn.String())
+	}
+}
+
+func TestExistsCommandOnMissingKey(t *testing.T) {
+	conn := getConnectionMock("*2\r\n$6\r\nEXISTS\r\n$7\r\nmissing\r\n")
+
+	handleConnection(conn)
+
+	if !strings.Contains(conn.String(), ":0") {
 		t.Errorf("expected response of command not found got response of %s", conn.String())
 	}
 }
