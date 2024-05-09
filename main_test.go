e55dae99d43b367acfb9ceb33e5115fb14026322
idugan100 <isaacd4444@gmail.com>
2024-05-08 22:41:29 -0400

    refactoring network connection mock creation out into a function

--- main_test.go
+++ main_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 )
 
+// this is a mock for the network connection that is read from and written to
 type TestingReadWriter struct {
 	*bytes.Buffer
 	*strings.Reader
@@ -15,123 +16,84 @@ func (rw TestingReadWriter) Read(p []byte) (n int, err error) {
 	return rw.Reader.Read(p)
 }
 
-// Write writes data to the underlying bytes.Buffer
 func (rw TestingReadWriter) Write(p []byte) (n int, err error) {
 	return rw.Buffer.Write(p)
 }
-func TestHandleGetSetCommands(t *testing.T) {
-	inputstring := "*3\r\n$3\r\nset\r\n$5\r\nhello\r\n$5\r\nworld\r\n"
-	inputStringReader := strings.NewReader(inputstring)
+
+func getTestingReadWriter(inputString string) TestingReadWriter {
+	inputStringReader := strings.NewReader(inputString)
 	var b bytes.Buffer
 
 	s := TestingReadWriter{
 		Buffer: &b,
 		Reader: inputStringReader,
 	}
+	return s
+}
+func TestHandleGetSetCommands(t *testing.T) {
+	conn := getTestingReadWriter("*3\r\n$3\r\nset\r\n$5\r\nhello\r\n$5\r\nworld\r\n")
 
-	handleConnection(s)
+	handleConnection(conn)
 
-	if !strings.Contains(b.String(), "OK") {
-		t.Errorf("expected response of ok got response of %s", b.String())
+	if !strings.Contains(conn.String(), "OK") {
+		t.Errorf("expected response of ok got response of %s", conn.String())
 	}
 
-	b.Reset()
-	inputstring = "*2\r\n$3\r\nget\r\n$5\r\nhello\r\n"
-	inputStringReader = strings.NewReader(inputstring)
+	conn1 := getTestingReadWriter("*2\r\n$3\r\nget\r\n$5\r\nhello\r\n")
 
-	s1 := TestingReadWriter{
-		Buffer: &b,
-		Reader: inputStringReader,
-	}
+	handleConnection(conn1)
 
-	handleConnection(s1)
-
-	if !strings.Contains(b.String(), "world") {
-		t.Errorf("expected response of world got response of %s", b.String())
+	if !strings.Contains(conn1.Buffer.String(), "world") {
+		t.Errorf("expected response of world got response of %s", conn1.Buffer.String())
 	}
 }
 
 func TestHandlePingCommand(t *testing.T) {
-	inputstring := "*1\r\n$4\r\nPING\r\n"
-	inputStringReader := strings.NewReader(inputstring)
-	var b bytes.Buffer
+	conn := getTestingReadWriter("*1\r\n$4\r\nPING\r\n")
 
-	s := TestingReadWriter{
-		Buffer: &b,
-		Reader: inputStringReader,
-	}
+	handleConnection(conn)
 
-	handleConnection(s)
-
-	if !strings.Contains(b.String(), "PONG") {
-		t.Errorf("expected response of PONG got response of %s", b.String())
+	if !strings.Contains(conn.String(), "PONG") {
+		t.Errorf("expected response of PONG got response of %s", conn.String())
 	}
 }
 
 func TestHandleLolWutCommand(t *testing.T) {
-	inputstring := "*1\r\n$6\r\nLOLWUT\r\n"
-	inputStringReader := strings.NewReader(inputstring)
-	var b bytes.Buffer
+	conn := getTestingReadWriter("*1\r\n$6\r\nLOLWUT\r\n")
 
-	s := TestingReadWriter{
-		Buffer: &b,
-		Reader: inputStringReader,
-	}
+	handleConnection(conn)
 
-	handleConnection(s)
-
-	if !strings.Contains(b.String(), ":):):)") {
-		t.Errorf("expected response of GoKV 0.1 :):):) got response of %s", b.String())
+	if !strings.Contains(conn.String(), ":):):)") {
+		t.Errorf("expected response of GoKV 0.1 :):):) got response of %s", conn.String())
 	}
 }
 
 func TestHandleSetCommendError(t *testing.T) {
-	inputstring := "*1\r\n$3\r\nset\r\n"
-	inputStringReader := strings.NewReader(inputstring)
-	var b bytes.Buffer
+	conn := getTestingReadWriter("*1\r\n$3\r\nset\r\n")
 
-	s := TestingReadWriter{
-		Buffer: &b,
-		Reader: inputStringReader,
-	}
+	handleConnection(conn)
 
-	handleConnection(s)
-
-	if !strings.Contains(b.String(), "incorrect") {
-		t.Errorf("expected response of incorrect number of args got response of %s", b.String())
+	if !strings.Contains(conn.String(), "incorrect") {
+		t.Errorf("expected response of incorrect number of args got response of %s", conn.String())
 	}
 }
 
 func TestHandleInvalidCommand(t *testing.T) {
-	inputstring := "*1\r\n$3\r\n123\r\n"
-	inputStringReader := strings.NewReader(inputstring)
-	var b bytes.Buffer
+	conn := getTestingReadWriter("*1\r\n$3\r\n123\r\n")
 
-	s := TestingReadWriter{
-		Buffer: &b,
-		Reader: inputStringReader,
-	}
+	handleConnection(conn)
 
-	handleConnection(s)
-
-	if !strings.Contains(b.String(), "command not found") {
-		t.Errorf("expected response of command not found got response of %s", b.String())
+	if !strings.Contains(conn.String(), "command not found") {
+		t.Errorf("expected response of command not found got response of %s", conn.String())
 	}
 }
 
 func TestHandleIndvalidCommand(t *testing.T) {
-	inputstring := "*1\r\n$3\r\n123\r\n"
-	inputStringReader := strings.NewReader(inputstring)
-	var b bytes.Buffer
+	conn := getTestingReadWriter("*1\r\n$3\r\n123\r\n")
 
-	s := TestingReadWriter{
-		Buffer: &b,
-		Reader: inputStringReader,
-	}
+	handleConnection(conn)
 
-	handleConnection(s)
-
-	if !strings.Contains(b.String(), "command not found") {
-		t.Errorf("expected response of command not found got response of %s", b.String())
+	if !strings.Contains(conn.String(), "command not found") {
+		t.Errorf("expected response of command not found got response of %s", conn.String())
 	}
 }
