a9e68d6d604044ef009ee19c6490326f2aeec2e5
idugan100 <isaacd4444@gmail.com>
2024-05-14 01:32:26 -0400

    adding support for hset and hget

--- handler.go
+++ handler.go
@@ -8,6 +8,8 @@ import (
 
 var setData = map[string]string{}
 var setMU = sync.RWMutex{}
+var HsetData = map[string]map[string]string{}
+var HsetMU = sync.RWMutex{}
 var Handlers = map[string]func([]Serializable) Serializable{
 	"PING":      ping,
 	"SET":       set,
@@ -19,6 +21,8 @@ var Handlers = map[string]func([]Serializable) Serializable{
 	"LOLWUT":    lolwut,
 	"FLUSHALL":  flushall,
 	"GETSET":    getset,
+	"HSET":      hset,
+	"HGET":      hget,
 }
 
 func ping(args []Serializable) Serializable {
@@ -144,5 +148,48 @@ func getset(args []Serializable) Serializable {
 	}
 
 	return Serializable{typ: "bulk", bulk: oldSerializable}
+
+}
+
+func hset(args []Serializable) Serializable {
+
+	if len(args)%2 != 1 || len(args) < 3 {
+		return Serializable{typ: "error", str: "incorrect number of arguements - must have hash key and then key value pairs"}
+	}
+	HsetMU.Lock()
+	HsetData[args[0].bulk] = map[string]string{}
+	for i := 1; i < len(args); i += 2 {
+		HsetData[args[0].bulk][args[i].bulk] = args[i+1].bulk
+	}
+	HsetMU.Unlock()
+
+	return Serializable{typ: "integer", num: (len(args) - 1) / 2}
+}
+
+func hget(args []Serializable) Serializable {
+	if len(args) < 2 {
+		return Serializable{typ: "error", str: "incorrect number of arguements - must have hash key and then value key"}
+	}
+
+	HsetMU.RLock()
+	defer HsetMU.RUnlock()
+
+	_, ok := HsetData[args[0].bulk]
+
+	if ok {
+		var resultList []Serializable
+		for i := 1; i < len(args); i++ {
+			value, ok := HsetData[args[0].bulk][args[i].bulk]
+			if ok {
+				resultList = append(resultList, Serializable{typ: "bulk", bulk: value})
+			} else {
+				resultList = append(resultList, Serializable{typ: "null"})
+			}
+		}
+		return Serializable{typ: "array", array: resultList}
+	} else {
+
+		return Serializable{typ: "null"}
+	}
 
 }
