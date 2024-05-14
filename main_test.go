ceaa2ea4c112a40d253cb1632dfe0b00c3fbd34e
idugan100 <isaacd4444@gmail.com>
2024-05-14 01:43:10 -0400

    adding tests to hset and hget

--- main_test.go
+++ main_test.go
@@ -166,3 +166,41 @@ func TestUnknownCommand(t *testing.T) {
 		t.Errorf("expected command not found. Got: %s", conn.String())
 	}
 }
+
+func TestHandleHSetandHGet(t *testing.T) {
+	conn := getConnectionMock("*8\r\n$4\r\nhset\r\n$6\r\nmyinfo\r\n$4\r\nname\r\n$5\r\nisaac\r\n$3\r\nage\r\n$2\r\n20\r\n$3\r\njob\r\n$3\r\nswe\r\n")
+
+	handleConnection(conn)
+
+	if !strings.Contains(conn.String(), "3") {
+		t.Errorf("expected response of 3 got response of %s", conn.String())
+	}
+	// hget myinfo name age job lol
+	conn1 := getConnectionMock("*6\r\n$4\r\nhget\r\n$6\r\nmyinfo\r\n$4\r\nname\r\n$3\r\nage\r\n$3\r\njob\r\n$3\r\nlol\r\n")
+
+	handleConnection(conn1)
+
+	if !strings.Contains(conn1.Buffer.String(), "isaac") || !strings.Contains(conn1.Buffer.String(), "20") || !strings.Contains(conn1.Buffer.String(), "swe") || !strings.Contains(conn1.Buffer.String(), "_") {
+		t.Errorf("expected response of 1) \"isaac\" 2) \"20\" 3) \"swe\" 4) (nil) got response of %s", conn1.Buffer.String())
+	}
+}
+
+func TestHandleHsetIncorrectNumberArgs(t *testing.T) {
+	conn := getConnectionMock("*3\r\n$4\r\nhset\r\n$6\r\nmyinfo\r\n$4\r\nname\r\n")
+
+	handleConnection(conn)
+
+	if !strings.Contains(conn.String(), "incorrect number") {
+		t.Errorf("expected error about incorrect number of arguements got response of %s", conn.String())
+	}
+}
+
+func TestHandleHgetIncorrectNumberArgs(t *testing.T) {
+	conn := getConnectionMock("*2\r\n$4\r\nhget\r\n$6\r\nmyinfo\r\n")
+
+	handleConnection(conn)
+
+	if !strings.Contains(conn.String(), "incorrect number") {
+		t.Errorf("expected error about incorrect number of arguements got response of %s", conn.String())
+	}
+}
